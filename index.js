f80aabc54efe1cef3c617bd89cde1b1b580e542a
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2021-09-30 15:53:33 -0400

    commit tree aa53d18c42e0f63b0b2383e227aae312be5b37d6

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+function findMatching(drivers, desired){
+    return drivers.filter(mightMatch => mightMatch.toLowerCase() === desired.toLowerCase()) 
+}
+function fuzzyMatch(drivers, desired){
+    return drivers.filter(mightMatch => mightMatch.toLowerCase().indexOf(desired.toLowerCase()) === 0)
+}
+
+function matchName (drivers, desired){
+    return drivers.filter(mightMatch => mightMatch.name === desired)
+}
